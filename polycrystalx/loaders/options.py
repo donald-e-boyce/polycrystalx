43c447cdc0684ea1646fc33977bb32daf806254c
donald e. boyce <donald.e.boyce@gmail.com>
2024-10-02 14:52:51 -0400

    options loader for elasticity

---
+++ polycrystalx/loaders/options.py
@@ -0,0 +1,14 @@
+"""Options loader module"""
+
+
+class LinearElasticity:
+    """Loader for linear elasticity
+
+    PARAMETERS
+    ----------
+    opts: input.options.LinearElasticity instance
+       options for this problem
+    """
+
+    def __init__(self, opts):
+        self.opts = opts
