7c209162b81fcdfd62f5eb96d3913be0c4e9bc39
donald e. boyce <donald.e.boyce@gmail.com>
2026-08-15 11:25:19 -0400

    added interpolate_to() function and EvalPoints class to utilities

--- polycrystalx/processes/__init__.py
+++ polycrystalx/processes/__init__.py
@@ -2,8 +2,6 @@
 from .linear_elasticity import LinearElasticity
 from .heat_transfer import HeatTransfer
 
-from ..utils import setup_output
-
 
 processes = (LinearElasticity, HeatTransfer)
 process_dict = {}
@@ -22,3 +20,27 @@ def run(job):
     outdir = setup_output(job.output_directory)
     process = process_dict[job.process](job)
     process.run(outdir)
+
+
+def setup_output(outdir):
+    """Make output directory if needed and return it as a Path.
+
+    Parameters
+    ----------
+    outdir: str or Path
+        name of output directory
+
+    Returns
+    -------
+    pathlib.Path
+        resolved output directory path
+    """
+    outdir = pathlib.Path(outdir)
+    print("output directory: ", outdir)
+    mpi_sync()
+    if not os.path.exists(outdir):
+        if myrank == 0:
+            log.log(log.LogLevel.INFO, f"creating output directory: {outdir}")
+            os.makedirs(outdir)
+        mpi_sync()
+    return outdir
